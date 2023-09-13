e0c3a663c541e4656f4193427361cc5782a5e6c5
Jay Cho <jaycho1214@gmail.com>
2023-09-13 12:38:28 +0900

    add sentry integration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
+import { SentryFilter } from './sentry.filter';
+import { ProfilingIntegration } from '@sentry/profiling-node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +14,18 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  Sentry.init({
+    dsn: 'https://a573363c4eafdf13505cf284fbdf7909@o4505870525136896.ingest.sentry.io/4505871026290688',
+    integrations: [new ProfilingIntegration()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  });
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
+
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   app.enableCors();
 
